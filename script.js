5fd5aaea1e78b0c32141f8ae8ae3a68eccfd7e49
AyMi@2025 <ayanmaiti558@gmail.com>
2026-06-23 16:14:16 +0530

    Selection of an Element

---
+++ script.js
@@ -0,0 +1,19 @@
+/* ====================================
+        Selection of an Element 
+   ==================================== */
+
+// const h1 = document.querySelector("h1") // Universal (Always use this)
+// console.dir(h1) ------------ dir = directory 
+
+// const h1 = document.querySelectorAll("h1") 
+// console.dir(h1) ----------- An Array (Nodelist)
+
+// const h1 = document.getElementsByTagName("h1") 
+// console.dir(h1) ----------- HTML Collections
+
+// const h1 = document.getElementById"h1") 
+// console.dir(h1) ----------- Give ID Name
+
+// const h1 = document.getElementByClassName"h1") 
+// console.dir(h1) ----------- Give Class Name
+
